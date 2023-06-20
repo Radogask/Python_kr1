a2f286d429af093e7208aee3d4ba6fb3f6159d68
Radogask <stas.k2001@mail.ru>
2023-06-20 17:52:08 +0400

    Добавьте файлы проекта.

---
+++ PythonApplication3/PythonApplication3.py
@@ -0,0 +1,10 @@
+﻿def factorials_sum(n):
+    factorial = 1 
+    sum = 0 
+    for i in range(1, n+1):
+        factorial *= i 
+        sum += factorial 
+    return sum
+
+n = int(input("Введите n: "))
+print("Сумма факториалов:", factorials_sum(n))
